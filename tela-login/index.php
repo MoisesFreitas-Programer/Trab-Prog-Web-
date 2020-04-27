1c4d4a62c1b820ed03c3ca934b4dea14b0b54c26
Renknarf Soares <58006101+Renknarf@users.noreply.github.com>
2020-04-27 19:45:22 -0400

    Update index.php

--- tela-login/index.php
+++ tela-login/index.php
@@ -11,7 +11,7 @@
 				<input type="email" placeholder="Usuário">
 				<input type="password" placeholder="Senha">
 				<input type="submit" placeholder="ACESSAR">
-				<a href=""> Ainda não possuicadastro?<strong>'Faça o seu!'</strong></a>
+				<a href=""> Ainda não possui cadastro?<strong>'Faça o seu!'</strong></a>
 			</form>
 	</div>
 	</body>

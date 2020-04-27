d23f57ad742003fcec298af832882d98124d43b9
Renknarf Soares <58006101+Renknarf@users.noreply.github.com>
2020-04-26 23:02:10 -0400

    Add files via upload

---
+++ tela-login/index.php
@@ -0,0 +1,18 @@
+<html lang="pt-br">
+	<head>
+		<meta charset="utf-8"/>
+		<title> Login (projeto)</title>
+		<link rel = "stylesheet" href="CSS/estilo.css">
+	</head> 
+	<body>
+	<div id="corpo-form">
+			<h1>Entrar</h1>			
+			<form method="POST" action="processa.php"> 
+				<imput type="email" placeholder="Usuário">
+				<imput type="password" placeholder="Senha">
+				<imput type="submit" placeholder="ACESSAR">
+				<a href=""> Ainda não possuicadastro?<strong>'Faça o seu!'</strong></a>
+			</form>
+	</div>
+	</body>
+</html>
